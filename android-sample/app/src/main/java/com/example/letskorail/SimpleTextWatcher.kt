d2483cfb0f2a5fd1110ee71e9b5cc0a64319989a
gooodchoi <jangu3394@gmail.com>
2026-02-19 14:00:32 +0900

    Update login flow UI with success/failure behaviors

---
+++ android-sample/app/src/main/java/com/example/letskorail/SimpleTextWatcher.kt
@@ -0,0 +1,14 @@
+package com.example.letskorail
+
+import android.text.Editable
+import android.text.TextWatcher
+
+class SimpleTextWatcher(private val afterTextChangedAction: () -> Unit) : TextWatcher {
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+
+    override fun afterTextChanged(s: Editable?) {
+        afterTextChangedAction.invoke()
+    }
+}
